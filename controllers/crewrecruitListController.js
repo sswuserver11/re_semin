b932e47b33d253c275e3644da3871df64efc1150
semin123 <si991014@naver.com>
2022-06-16 09:28:52 +0900

    commit

--- controllers/crewrecruitListController.js
+++ controllers/crewrecruitListController.js
@@ -3,7 +3,7 @@ const db = require("../models/index"),//다 가져와서
     Op = db.Sequelize.Op;
 
 exports.showcrewrecruitList = async (req,res) => {
-    try{
+    try{  
             data = await CrewRecruit.findAll();
             res.locals.data = data;
             res.render("crewrecruitList",{crewrecruit: data});
